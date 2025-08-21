8f191726bd5acc941ca8b276e2cc53377d9efe5a
Sarah Gaichas <sarah.gaichas@noaa.gov>
2025-08-20 20:28:30 -0400

    text wrangling part 1

--- R/scrape_ecodata_catalog.R
+++ R/scrape_ecodata_catalog.R
@@ -1,4 +1,4 @@
-#' Scrape ecodata catalog pages
+#' Scrape ecodata catalog page
 #'
 #' Pulls selected fields, paragraphs, and images from the NOAA-EDAB Indicator Catalog website.
 #' Code had to be tailored to this website because not all fields come under standard headers.
@@ -11,7 +11,7 @@
 #'@examples
 #'  url <- "https://noaa-edab.github.io/catalog/trans_dates.html"
 #'  FieldList <- c("Description", "Introduction", "Key Results",
-#'                  "Spatial scale", "Temporal scale", "Implications".
+#'                  "Spatial scale", "Temporal scale", "Implications",
 #'                  "ecodata name", "tech-doc link")
 #'  scrape_ecodata_catalog(url, FieldList)
 #'
@@ -28,12 +28,28 @@ scrape_ecodata_catalog <- function(url, FieldList){
   chnum <- as.numeric(gsub(".*?([0-9]+).*", "\\1", titles[2]))
   name <- stringr::str_trim(gsub("[0-9]", "", titles[2]))
 
+  h2fields <- page |>
+    rvest::html_elements("h2") |>
+    rvest::html_text2()
+
   # get level 2 headings and paragraphs, our data are in both
   fields <- page |>
     rvest::html_elements("h2, p") |>
     rvest::html_text2()
 
   # filter by the fieldlist
+  matching_fields <- fields[stringr::str_detect(fields, paste(FieldList, collapse = "|"))]
+
+  # capture text sections following an h2 field
+  fieldind <- which(stringr::str_detect(fields, paste(FieldList, collapse = "|")))
+  headind <- which(stringr::str_detect(fields, paste(h2fields, collapse = "|")))
+  h2fieldind <- intersect(fieldind, headind)
+  fieldtext <- fields[h2fieldind+1]
+
+  # split into column names and values
+  # two patterns: column name : value
+  # and section number column name with value from fieldtext
+
 
   # return result with named columns
 
