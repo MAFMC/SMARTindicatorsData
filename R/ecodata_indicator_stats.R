1e69cdd427aae97468297a5613475d328741ade9
Sarah Gaichas <sarah.gaichas@noaa.gov>
2025-08-22 14:52:25 -0400

    WIP ecodata stats

--- R/ecodata_indicator_stats.R
+++ R/ecodata_indicator_stats.R
@@ -16,7 +16,7 @@
 #'
 #'@examples
 #'  dataset <- ecodata::forage_index
-#'  StatList <- c("Units", "StartYear", "EndYear", "TSMean", "TS95CI")
+#'  StatList <- c("Units", "EPUs", "StartYear", "EndYear", "TSMean", "TSMin", "TSMax", "YrMin", "YrMax")
 #'  ecodata_indicator_stats(dataset, StatList)
 #'
 #'@export
@@ -24,40 +24,68 @@ ecodata_indicator_stats <- function(dataset, StatList = NULL){
 
   # default StatList for iterating with purrr:map
   if(is.null(StatList)){
-    StatList <- c("Units", "StartYear", "EndYear", "TSMean", "TS95CI")
+    StatList <- c("Units", "EPUs", "StartYear", "EndYear", "TSMean", "TSMin", "TSMax", "YrMin", "YrMax")
   }
 
   # load the dataset
   dat <- dataset
 
+  # check names and force to a standard
+  names(dat) <- stringr::str_to_title(names(dat))
+
   # for each variable get name and units
   summ <- data.frame(Indicator = unique(dat$Var),
                      Dataset = deparse(substitute(dataset)))
 
+#  if("Units" %in% StatList){
+
+  if(!"Units" %in% names(dat)) dat$Units <- "no Units field"
+
   Units <- dat |>
     dplyr::group_by(Var) |>
-    dplyr::select(Units) |>
+    dplyr::select(Var, Units) |>
+    dplyr::distinct() |>
+    dplyr::ungroup()
+#  }
+
+  # spatial
+
+#  if("EPUs" %in% StatList){
+
+  if(!"EPU" %in% names(dat)) dat$EPU <- "no EPU field"
+
+  EPUs <- dat |>
+    dplyr::group_by(Var) |>
+    dplyr::select(Var, EPU) |>
     dplyr::distinct()
 
+#  }
+
   # for each variable get start and end year or time
+  if(!"Time" %in% names(dat)) dat$Time <- "no Time field"
+
   StartEnd <- dat |>
     dplyr::group_by(Var) |>
-    dplyr::select(Time) |>
-    dplyr::summarise(StartYear = min(Time),
-                     EndYear = max(Time),
+    dplyr::select(Var, Time) |>
+    dplyr::summarise(StartYear = min(Time, na.rm = T),
+                     EndYear = max(Time, na.rm = T),
                      EstYrs = EndYear-StartYear+1,
                      ActualYrs = dplyr::n_distinct(Time))
 
-  # mean and 95% CI of time series
+  # mean and range of time series with min and max value years
   TSstats <- dat |>
-    dplyr::group_by(Var) |>
-
-
-
-  # test for trends or time blocks?
+    dplyr::group_by(Var, EPU) |>
+    dplyr::select(Var, Time, Value) |>
+    dplyr::summarise(TSMean = mean(Value, na.rm = T),
+                     TSMin = min(Value, na.rm = T),
+                     TSMax = max(Value, na.rm = T),
+                     YrMin = Time[which(Value == TSMin)],
+                     YrMax = Time[which(Value == TSMax)])
 
   # collate all this and return a dataframe
+  sumstats <- dplyr::left_join(summ, Units, by=c("Indicator"="Var")) |>
+    dplyr::left_join(EPUs, by=c("Indicator"="Var"))
 
-  return(summ)
+  return(sumstats)
 }
 
