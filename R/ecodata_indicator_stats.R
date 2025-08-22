a467e5ad153e3fa7a01cafc6c0187538ae4d940e
Sarah Gaichas <sarah.gaichas@noaa.gov>
2025-08-22 18:33:28 -0400

    adds error handling but not enough

--- R/ecodata_indicator_stats.R
+++ R/ecodata_indicator_stats.R
@@ -2,71 +2,84 @@
 #'
 #' Retrieve attributes of an ecodata indicator and return as a dataframe, and plot the indicator.
 #'
-#'@param dataset The ecodata indicator dataset name (may contain one or multiple indicator variables)
-#'@param StatList Optional, A character vector of attributes and statistics to be calculated.
-#'The default StatList is c("Units", "StartYear", "EndYear", "TSMean", "TS95CI")
+#'@param dataset The ecodata indicator dataset name in ecodata::dataset format (may contain one or multiple indicator variables)
 #'
 #'@return a dataframe with variables from the StatList and values calculated from the indicator(s). Columns:
 #'\itemize{
 #'  \item{\code{Indicator}, name of the variable summarized from the ecodata indicator dataset}
 #'  \item{\code{Dataset}, the ecodata indicator dataset name = "ecodata name" collected from catalog}
-#'  \item{\code{Varname}, variable names corresponding the StatList}
-#'  \item{\code{Value}, value of the extracted or calculated variable}
+#'  \item{\code{Units}, the units of the indicator}
+#'  \item{\code{EPU}, the Ecosystem Production Unit where the indicator is measured; }
+#'  \item{\code{Varname}, basic indicator statistics; StartYear is the first year of the time series,
+#'  EndYear is the last year of the time series, EstYrs is the total possible years between the start
+#'  and end years, ActualYrs is the count of years to determine if any are missing, TSMean is the time
+#'  series mean value, TSmin is the time series minimum value, TSmax is the time series maximum value,
+#'  YrTSmin is the year of the time series minimum value, YrTSmax is the year of the time series maximum value}
+#'  \item{\code{Value}, value of the statistic for the indicator}
 #' }
 #'
 #'@examples
-#'  dataset <- ecodata::forage_index
-#'  StatList <- c("Units", "EPUs", "StartYear", "EndYear", "TSMean", "TSMin", "TSMax", "YrMin", "YrMax")
-#'  ecodata_indicator_stats(dataset, StatList)
+#'  ecodata_indicator_stats(dataset = ecodata::forage_index)
 #'
 #'@export
-ecodata_indicator_stats <- function(dataset, StatList = NULL){
+ecodata_indicator_stats <- function(dataset){
 
-  # default StatList for iterating with purrr:map
-  if(is.null(StatList)){
-    StatList <- c("Units", "EPUs", "StartYear", "EndYear", "TSMean", "TSMin", "TSMax", "YrMin", "YrMax")
-  }
+  # These will be hardcoded for now
+  # # default StatList for iterating with purrr:map
+  # if(is.null(StatList)){
+  #   StatList <- c("Units", "EPUs", "StartYear", "EndYear", "TSMean", "TSMin", "TSMax", "YrMin", "YrMax")
+  # }
 
   # load the dataset
   dat <- dataset
-
-  # check names and force to a standard
-  names(dat) <- stringr::str_to_title(names(dat))
 
   # for each variable get name and units
   summ <- data.frame(Indicator = unique(dat$Var),
                      Dataset = deparse(substitute(dataset)))
 
-#  if("Units" %in% StatList){
-
   if(!"Units" %in% names(dat)) dat$Units <- "no Units field"
 
   Units <- dat |>
     dplyr::group_by(Var) |>
-    dplyr::select(Var, Units) |>
+    dplyr::select(Indicator = Var, Units) |>
+    dplyr::filter(!is.na(Units)) |>
     dplyr::distinct() |>
     dplyr::ungroup()
-#  }
+
 
   # spatial
-
-#  if("EPUs" %in% StatList){
-
   if(!"EPU" %in% names(dat)) dat$EPU <- "no EPU field"
 
   EPUs <- dat |>
     dplyr::group_by(Var) |>
-    dplyr::select(Var, EPU) |>
+    dplyr::select(Indicator = Var, EPU) |>
     dplyr::distinct()
 
-#  }
-
   # for each variable get start and end year or time
-  if(!"Time" %in% names(dat)) dat$Time <- "no Time field"
+  # if no Time column stop here
+  if(!"Time" %in% names(dat)){
+    dat$Time <- "no Time field"
+    notime <- dat |>
+      dplyr::group_by(Var) |>
+      dplyr::select(Indicator = Var, Time) |>
+      dplyr::distinct()
+
+    columns <- dplyr::left_join(summ, Units) |>
+      dplyr::left_join(EPUs) |>
+      dplyr::left_join(notime)
+
+    return(columns)
+  }
+
+  # if time is not numeric--chl_pp et al--pull out the year
+  if(is.character(dat$Time)){
+    dat$Time <- as.numeric(gsub("[^0-9.-]", "", dat$Time))
+  }
 
   StartEnd <- dat |>
-    dplyr::group_by(Var) |>
-    dplyr::select(Var, Time) |>
+    dplyr::group_by(Var, EPU) |>
+    dplyr::filter(!is.na(Value)) |>
+    dplyr::select(Indicator = Var, EPU, Time) |>
     dplyr::summarise(StartYear = min(Time, na.rm = T),
                      EndYear = max(Time, na.rm = T),
                      EstYrs = EndYear-StartYear+1,
@@ -75,16 +88,23 @@ ecodata_indicator_stats <- function(dataset, StatList = NULL){
   # mean and range of time series with min and max value years
   TSstats <- dat |>
     dplyr::group_by(Var, EPU) |>
-    dplyr::select(Var, Time, Value) |>
+    dplyr::filter(!is.na(Value)) |>
+    dplyr::select(Indicator = Var, EPU, Time, Value) |>
     dplyr::summarise(TSMean = mean(Value, na.rm = T),
                      TSMin = min(Value, na.rm = T),
                      TSMax = max(Value, na.rm = T),
-                     YrMin = Time[which(Value == TSMin)],
-                     YrMax = Time[which(Value == TSMax)])
+                     YrTSMin = Time[which(Value == TSMin)][1],  #take first if >1
+                     YrTSMax = Time[which(Value == TSMax)][1]) #take first if >1
 
   # collate all this and return a dataframe
-  sumstats <- dplyr::left_join(summ, Units, by=c("Indicator"="Var")) |>
-    dplyr::left_join(EPUs, by=c("Indicator"="Var"))
+  columns <- dplyr::left_join(summ, Units) |>
+    dplyr::left_join(EPUs)
+
+  stats <- merge(StartEnd, TSstats) |>
+    tidyr::pivot_longer(-c(Indicator, EPU), names_to = "Varname", values_to = "Value")
+
+  sumstats <- merge(columns, stats) |>
+    dplyr::select(Indicator, Dataset, Units, EPU, Varname, Value)
 
   return(sumstats)
 }
